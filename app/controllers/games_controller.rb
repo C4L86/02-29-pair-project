0700cc3a71a58baa56fc43c3bc5859ec3d507d33
Chris Foral <chris.foral@gmail.com>
2016-03-01 11:35:43 -0600

    Library view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,3 +2,27 @@ MyApp.get "/games_new" do
 
   erb :"admin/games_new"
 end
+
+MyApp.post "/process_add_games" do
+  @game = Game.new
+
+  @game.title = params["title"]
+  @game.genre = params["genre"]
+  @game.age_group = params["age_group"].to_i
+  @game.min_players = params["min_players"].to_i
+  @game.max_players = params["max_players"].to_i
+  @game.description = params["description"]
+  @game.play_time = params["play_time"].to_i
+  @game.publisher = params["publisher"]
+  @game.price = params["price"].to_f
+
+  @game.save
+
+  redirect "/games_new"
+end
+
+MyApp.get "/index" do
+  @games = Game.all
+
+  erb :"/admin/index"
+end
