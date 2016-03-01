74b041fc57c83501364dc789f6d9b47134f70ecd
Chris Foral <chris.foral@gmail.com>
2016-03-01 13:23:59 -0600

    CRUD done for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,15 +6,15 @@ end
 MyApp.post "/process_add_games" do
   @game = Game.new
 
-  @game.title = params["title"]
-  @game.genre = params["genre"]
-  @game.age_group = params["age_group"].to_i
+  @game.title       = params["title"]
+  @game.genre       = params["genre"]
+  @game.age_group   = params["age_group"].to_i
   @game.min_players = params["min_players"].to_i
   @game.max_players = params["max_players"].to_i
   @game.description = params["description"]
-  @game.play_time = params["play_time"].to_i
-  @game.publisher = params["publisher"]
-  @game.price = params["price"].to_f
+  @game.play_time   = params["play_time"].to_i
+  @game.publisher   = params["publisher"]
+  @game.price       = params["price"].to_f
 
   @game.save
 
@@ -25,4 +25,38 @@ MyApp.get "/index" do
   @games = Game.all
 
   erb :"/admin/index"
+end
+
+MyApp.get "/games_update/:game_id" do
+  @games = Game.all
+  @game  = Game.find_by_id(params[:game_id])
+
+  erb :"/admin/games_update"
+end
+
+MyApp.post "/process_games_update/:game_id" do
+  @games = Game.all
+  @game =  Game.find_by_id(params[:game_id])
+
+  @game.title       = params["title"]
+  @game.genre       = params["genre"]
+  @game.age_group   = params["age_group"].to_i
+  @game.min_players = params["min_players"].to_i
+  @game.max_players = params["max_players"].to_i
+  @game.description = params["description"]
+  @game.play_time   = params["play_time"].to_i
+  @game.publisher   = params["publisher"]
+  @game.price       = params["price"].to_f
+
+  @game.save
+
+  redirect "/index"
+end
+
+MyApp.get "/games_delete/:game_id" do
+  @game = Game.find_by_id(params[:game_id])
+
+  @game.delete
+
+  redirect "/index"
 end
