d5e63f6e082cb98a4132385575a6e6284101be75
Chris Foral <chris.foral@gmail.com>
2016-03-01 18:03:18 -0600

    Search basic functionality, kind of

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,7 +9,7 @@ MyApp.post "/process_game_search" do
   game_arr = params.values
 
   
-  @game = Game.where({"title" => game_arr[0]})
+  @game = Game.where({"title" => game_arr[0]}) || Game.where({"genre" => game_arr[1]}) || Game.where({"age_group" => game_arr[2]}) || Game.where({"min_players" => game_arr[3]}) || Game.where({"max_players" => game_arr[4]}) || Game.where({"play_time" => game_arr[5]})
   binding.pry
   erb :"/user/game_search_result"
 end
