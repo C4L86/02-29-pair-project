721aae24d0e08e05083ff24c30a612c5ca6bad3d
Chris Foral <chris.foral@gmail.com>
2016-03-02 10:32:31 -0600

    printing results error

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,21 +1,23 @@
 MyApp.get "/" do
-  
+  @games = Game.all
+  @game
+
   erb :"/user/game_search"
 end
 
 MyApp.post "/process_game_search" do
   @games = Game.all
   
-  search_hash = {}
+  @search_hash = {}
   
   params.each do |key,value|
     if value != ""
-      search_hash[key] = value
+      @search_hash[key] = value
     else
     end
   end
-  
-  @game = Game.where(search_hash)
 
-  erb :"/user/game_search_result"
+  @game = Game.where(@search_hash)
+
+  redirect "/"
 end
