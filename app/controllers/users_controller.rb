9903f310dcc729853ff1b4393b03de456e13ed77
Chris Foral <chris.foral@gmail.com>
2016-03-02 13:54:21 -0600

    user profile and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,3 +45,30 @@ MyApp.get "/logout_user/:user_id" do
     erb :"logins/logout_error"
   end
 end
+
+MyApp.get "/user_profile" do
+  @users = User.all
+  @user = User.find_by_id(session["user_id"])
+
+  erb :"/user/user_profile"
+end
+
+MyApp.get "/user_update" do
+  @user = User.find_by_id(session["user_id"])
+
+  erb :"/user/user_update_form"
+end
+
+MyApp.post "/process_user_update_form" do
+  @user = User.find_by_id(session["user_id"])
+
+  @user.name     = params["name"]
+  @user.email    = params["email"]
+  @user.dob      = params["dob"]
+  @user.password = params["password"]
+
+  @user.save
+  session["user_id"] = @user.id
+
+  redirect "/user_profile"
+end
