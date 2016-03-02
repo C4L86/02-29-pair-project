1a1f5e5c799d4ede96e93bfae61c55673792cf14
Chris Foral <chris.foral@gmail.com>
2016-03-01 22:02:47 -0600

    Search function functioning

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,11 +5,14 @@ end
 
 MyApp.post "/process_game_search" do
   @games = Game.all
-
-  game_arr = params.values
-
+  search_hash = {}
+  params.each do |key,value|
+    if value != ""
+      search_hash[key] = value
+    else
+    end
+  end
+  @game = Game.where(search_hash)
   
-  @game = Game.where({"title" => game_arr[0]}) || Game.where({"genre" => game_arr[1]}) || Game.where({"age_group" => game_arr[2]}) || Game.where({"min_players" => game_arr[3]}) || Game.where({"max_players" => game_arr[4]}) || Game.where({"play_time" => game_arr[5]})
-  binding.pry
   erb :"/user/game_search_result"
 end
