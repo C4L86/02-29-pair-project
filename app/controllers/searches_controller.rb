eb0c9c913c44d8a8062df76f46fe6404b2e0877a
Chris Foral <chris.foral@gmail.com>
2016-03-02 08:51:51 -0600

    .

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,7 +5,6 @@ end
 
 MyApp.post "/process_game_search" do
   @games = Game.all
-<<<<<<< HEAD
   
   search_hash = {}
   
@@ -18,13 +17,5 @@ MyApp.post "/process_game_search" do
   
   @game = Game.where(search_hash)
 
-=======
-
-  game_arr = params.values
-
-  
-  @game = Game.where({"title" => game_arr[0]})
-  binding.pry
->>>>>>> master
   erb :"/user/game_search_result"
 end
