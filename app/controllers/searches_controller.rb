54ca261883c86c9e701c42ad5167280aef5442b0
Chris Foral <chris.foral@gmail.com>
2016-03-02 16:19:44 -0600

    Sumeet's class method added and functioning

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,16 +10,7 @@ end
 MyApp.post "/process_game_search" do
   @games = Game.all
   
-  @search_hash = {}
-  
-  params.each do |key,value|
-    if value != ""
-      @search_hash[key] = value
-    else
-    end
-  end
-
-  @game = Game.where(@search_hash)
+  @game = Game.smart_search(params)
 
   erb :"/user/game_search_result"
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,15 @@
 class Game < ActiveRecord::Base
+  def self.smart_search(params)
+    @search_hash = {}
+    
+    params.each do |key,value|
+      if value != ""
+        @search_hash[key] = value
+      else
+      end
+    end
+
+    Game.where(@search_hash)
+  end  
 
 end
