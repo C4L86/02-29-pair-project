ba8eabe064ddd5a7df0ce32cfdd38d64641e31ba
Chris Foral <chris.foral@gmail.com>
2016-03-03 17:25:10 -0600

    working on fetch_titles_from_spielbound method

--- lib/spielbound_title_scraper.rb
+++ lib/spielbound_title_scraper.rb
@@ -8,54 +8,45 @@
 # => ...
 
 require "httparty"
+require "Nokogiri"
 
-def fetch_game_info(title)
-  bgg_data_for_title = HTTParty.get("http://boardgamegeek.com/xmlapi/search?search=#{title}")
+# def fetch_game_info(title)
+#   bgg_data_for_title = HTTParty.get("http://boardgamegeek.com/xmlapi/search?search=#{title}")
 
-  id = bgg_data_for_title["boardgames"]["boardgame"][0]["objectid"]
+#   id = bgg_data_for_title["boardgames"]["boardgame"][0]["objectid"]
 
-  game_data = HTTParty.get("http://www.boardgamegeek.com/xmlapi/boardgame/#{id}")
+#   game_data = HTTParty.get("http://www.boardgamegeek.com/xmlapi/boardgame/#{id}")
 
-  title        = game_data["boardgames"]["boardgame"]["name"][0]["__content__"]
-  min_players  = game_data["boardgames"]["boardgame"]["minplayers"]
-  max_players  = game_data["boardgames"]["boardgame"]["maxplayers"]
-  min_playtime = game_data["boardgames"]["boardgame"]["minplaytime"]
-  max_playtime = game_data["boardgames"]["boardgame"]["maxplaytime"]
-  age_group    = game_data["boardgames"]["boardgame"]["age"]
-  description  = game_data["boardgames"]["boardgame"]["description"]
-  image        = game_data["boardgames"]["boardgame"]["image"]
-  publisher    = game_data["boardgames"]["boardgame"]["boardgamepublisher"][0]["__content__"]
+#   title        = game_data["boardgames"]["boardgame"]["name"][0]["__content__"]
+#   min_players  = game_data["boardgames"]["boardgame"]["minplayers"]
+#   max_players  = game_data["boardgames"]["boardgame"]["maxplayers"]
+#   min_playtime = game_data["boardgames"]["boardgame"]["minplaytime"]
+#   max_playtime = game_data["boardgames"]["boardgame"]["maxplaytime"]
+#   age_group    = game_data["boardgames"]["boardgame"]["age"]
+#   description  = game_data["boardgames"]["boardgame"]["description"]
+#   image        = game_data["boardgames"]["boardgame"]["image"]
+#   publisher    = game_data["boardgames"]["boardgame"]["boardgamepublisher"][0]["__content__"]
 
-  # genre      = 
-end
+#   # genre      = 
+# end
 
 # puts fetch_game_info("Agricola")
 
-# class GameDataScraper
-#   def titles
-#     return @titles
-#   end
+def fetch_titles_from_spielbound
+  title = []
+  current_page = 1
+  url = "http://spielbound.com/library?title=&field_playing_time_min__value=&&p=All&min=All&max=All&rating=All&sort_by=title&sort_order=ASC&page=#{current_page}"
 
-#   def fetch_titles
-#     @titles = []
+  while (url) do  
+    doc = Nokogiri::HTML(open("http://spielbound.com/library?title=&field_playing_time_min__value=&&p=All&min=All&max=All&rating=All&sort_by=title&sort_order=ASC&page=#{current_page}"))
 
-#     current_page = 1
-#     # Go to SB site
-    # response = HTTParty.get("http://spielbound.com/library?title=&field_playing_time_min__value=&&p=All&min=All&max=All&rating=All&sort_by=title&sort_order=ASC&page=#{current_page}")
-
-    # # search for div class="gamecard"
-    # # within this div class, search for class="name"
-    # # within that class, search for the href containing the title
-    # # within that href, grab the title which is viewable to the user as the href button
-    # response[<div class="gamecard">][<h2 class="name">][<a href="http://spielbound.com/game-title/GAME_TITLE">GAME_TITLE_TO_GRAB</a></h2>]
-    # # grab that title
-    # # push the title into @titles Array
-    # @titles << title
-    # # iterate 'current_page' up 1 to move to the next page
-    # current_page += 1
-    # loop through again until reaching the end of the final page
+    doc.css("div.gamecard").each do |gamecard|
+      title << gamecard.css("h2.name a").text
+    end
+    current_page += 1
   end
+
+  return title
 end
 
-# scraper = SpielboundTitleScraper.new
-# scraper.fetch_titles
+puts fetch_titles_from_spielbound
