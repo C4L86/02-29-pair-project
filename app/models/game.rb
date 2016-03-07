2985250331b948d7d8bcbbb2cfbf3f01eeb2424d
Patrick Clark <patclark1958@gmail.com>
2016-03-07 13:34:36 -0600

    tom doc for Game class

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
+#Game class refers to the games table in the spielbound_search database. self.smart_search queries the games table based on user inputs. 
+#
+#Inputs: The games table is populated as follows: 1 - the titles are raked from the html code on SpielBound's website using nokogiri and placed in a text file. 2 - The game profile information is from the free API of Board Game Geek, using HTTParty.
+#
+#Returns an ActiveRecord relation containing all matches with the search parameters. 
+
 class Game < ActiveRecord::Base
   def self.smart_search(params)
     if params["title"].blank?
