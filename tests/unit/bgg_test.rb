3e31d85ff7da948b664b08e0fcb5a8e8e619290c
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-07 16:40:28 -0600

    Add blank tests and remove old tests. Cleaner demo this way.

---
+++ tests/unit/bgg_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+# Tests the BoardGameGeek service.
+class BoardGameGeekTest < Minitest::Test
+  def setup
+    super
+  end
+
+  # def test_something
+  # end
+end
---
+++ tests/unit/spielbound_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+# Tests the Spielbound service.
+class SpielboundTest < Minitest::Test
+  def setup
+    super
+  end
+
+  # def test_something
+  # end
+end
