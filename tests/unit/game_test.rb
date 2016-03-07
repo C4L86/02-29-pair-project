a30017173b6a3454917804172d5eaa90f56f0dfa
Patrick Clark <patclark1958@gmail.com>
2016-03-07 15:55:08 -0600

    not thrilled with solution

--- tests/unit/game_test.rb
+++ tests/unit/game_test.rb
@@ -9,6 +9,8 @@ class GameTest < Minitest::Test
   def setup
     super
 
+
+
     @game             = Game.new
     @game.title       = "Test Title"
     @game.genre       = "Test Genre"
@@ -22,17 +24,37 @@ class GameTest < Minitest::Test
     @game.save
   end
 
-  def test_smart_search
+  # def test_smart_search
+  #   assert_includes(Game.smart_search({"genre"=>"Test Genre","title"=>""}), @game)
+  #   assert_includes(Game.smart_search({"title"=>"Test Title","genre"=>""}), @game)
+  #   assert_includes(Game.smart_search({"age_group"=>"8","genre"=>""}), @game)
+  #   assert_includes(Game.smart_search({"min_players"=>"1","age_group"=>""}), @game)
+  #   assert_includes(Game.smart_search({"max_players"=>"2","min_players"=>""}), @game)
+  #   assert_includes(Game.smart_search({"play_time"=>"90","max_players"=>""}), @game)
+  #   assert_includes(Game.smart_search({"description"=>"Test Description","play_time"=>""}), @game)
+  #   assert_includes(Game.smart_search({"publisher"=>"Test Publisher","description"=>""}), @game)
+  #   assert_includes(Game.smart_search({"price"=>"49.99","publisher"=>""}), @game)
+  #   refute_includes(Game.smart_search({"price"=>"Dinosaur","publisher"=>"49.99"}), @game)
+  #   refute_includes(Game.smart_search({"title"=>"90","play_time"=>"Dinosaur"}), @game)
+  # end
+
+  def test_smart_search_1
     assert_includes(Game.smart_search({"genre"=>"Test Genre","title"=>""}), @game)
     assert_includes(Game.smart_search({"title"=>"Test Title","genre"=>""}), @game)
     assert_includes(Game.smart_search({"age_group"=>"8","genre"=>""}), @game)
+    refute_includes(Game.smart_search({"title"=>"90","play_time"=>"Dinosaur"}), @game)
+  end
+
+  def test_smart_search_2
     assert_includes(Game.smart_search({"min_players"=>"1","age_group"=>""}), @game)
     assert_includes(Game.smart_search({"max_players"=>"2","min_players"=>""}), @game)
     assert_includes(Game.smart_search({"play_time"=>"90","max_players"=>""}), @game)
+  end
+
+  def test_smart_search_3
     assert_includes(Game.smart_search({"description"=>"Test Description","play_time"=>""}), @game)
     assert_includes(Game.smart_search({"publisher"=>"Test Publisher","description"=>""}), @game)
     assert_includes(Game.smart_search({"price"=>"49.99","publisher"=>""}), @game)
     refute_includes(Game.smart_search({"price"=>"Dinosaur","publisher"=>"49.99"}), @game)
-    refute_includes(Game.smart_search({"title"=>"90","play_time"=>"Dinosaur"}), @game)
   end
 end
