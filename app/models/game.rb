1f1279707bc8610dedbf9883d0d2b382eb628418
Chris Foral <chris.foral@gmail.com>
2016-03-07 16:26:09 -0600

    Took care of 'DuplicateMethodCall' wanrings

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,42 +4,47 @@
 #Returns an ActiveRecord relation containing all matches with the search parameters. 
 class Game < ActiveRecord::Base
   def smart_search_title
-    if params["title"].blank?
+    params["title"] = title_params
+    if title_params.blank?
       title = ""
     else
-      title = "title='#{params["title"]}' AND "
+      title = "title='#{title_params}' AND "
     end
   end
 
   def smart_search_age_group
-    if params["age_group"].blank?
+    params["age_group"] = age_group_params
+    if age_group_params.blank?
       age_group = ""
     else
-      age_group = "age_group >= #{params["age_group"]} AND "
+      age_group = "age_group >= #{age_group_params} AND "
     end
   end
 
   def smart_search_min_players
-    if params["min_players"].blank?
+    params["min_players"] = min_players_params
+    if min_players_params.blank?
       min_players = ""
     else
-      min_players = "min_players >= #{params["min_players"]} AND "
+      min_players = "min_players >= #{min_players_params} AND "
     end
   end
 
   def smart_search_max_players
-    if params["max_players"].blank?
+    params["max_players"] = max_players_params
+    if max_players_params.blank?
       max_players = ""
     else
-      max_players = "max_players <= #{params["max_players"]} AND "
+      max_players = "max_players <= #{max_players_params} AND "
     end
   end
 
   def smart_search_play_time
-    if params["play_time"].blank?
+    params["play_time"] = play_time_params
+    if play_time_params.blank?
       max_playtime = ""
     else
-      max_playtime = "max_playtime <= #{params["play_time"]} AND "
+      max_playtime = "max_playtime <= #{play_time_params} AND "
     end
   end
 
