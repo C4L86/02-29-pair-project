79b821cfcfea024e7a9118d889d3586bc88f72b3
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-08 07:07:33 -0600

    Test multiple pages of results for Spielbound scraper

--- tests/unit/spielbound_test.rb
+++ tests/unit/spielbound_test.rb
@@ -6,11 +6,21 @@ class SpielboundTest < Minitest::Test
     super
   end
 
-  def test_something
+  def test_single_page_of_results
     fetch_titles_from_spielbound(1)
     titles_in_file = open("titles.txt", 'r').read
 
     assert_includes(titles_in_file, "7 Wonders: Duel\n")
     assert_includes(titles_in_file, "Acquire\n")
+  end
+
+  def test_multiple_pages_of_results
+    fetch_titles_from_spielbound(2)
+    titles_in_file = open("titles.txt", 'r').read
+
+    assert_includes(titles_in_file, "7 Wonders: Duel\n")
+    assert_includes(titles_in_file, "Acquire\n")
+    assert_includes(titles_in_file, "Agricola\n")
+    assert_includes(titles_in_file, "Alchemists\n")
   end
 end
