8c58553c36755ed1b08954d3d9f7053664a3f1e4
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-08 10:18:43 -0600

    Moved methods into class

--- lib/board_game_geek.rb
+++ lib/board_game_geek.rb
@@ -1,45 +1,47 @@
-def create_game(title, game_data)
-  @game = Game.new
+class BoardGameGeek
+  def create_game(title, game_data)
+    @game = Game.new
 
-  @game.title        = title
-  @game.min_players  = game_data["boardgames"]["boardgame"]["minplayers"]
-  @game.max_players  = game_data["boardgames"]["boardgame"]["maxplayers"]
-  @game.min_playtime = game_data["boardgames"]["boardgame"]["minplaytime"]
-  @game.max_playtime = game_data["boardgames"]["boardgame"]["maxplaytime"]
-  @game.age_group    = game_data["boardgames"]["boardgame"]["age"]
-  @game.description  = game_data["boardgames"]["boardgame"]["description"]
-  @game.image        = game_data["boardgames"]["boardgame"]["image"]
+    @game.title        = title
+    @game.min_players  = game_data["boardgames"]["boardgame"]["minplayers"]
+    @game.max_players  = game_data["boardgames"]["boardgame"]["maxplayers"]
+    @game.min_playtime = game_data["boardgames"]["boardgame"]["minplaytime"]
+    @game.max_playtime = game_data["boardgames"]["boardgame"]["maxplaytime"]
+    @game.age_group    = game_data["boardgames"]["boardgame"]["age"]
+    @game.description  = game_data["boardgames"]["boardgame"]["description"]
+    @game.image        = game_data["boardgames"]["boardgame"]["image"]
 
-  @game.save
-end
+    @game.save
+  end
 
-def bgg_data_for_title(title)
-  HTTParty.get("http://boardgamegeek.com/xmlapi/search?search=#{title}&exact=1")
-end
+  def bgg_data_for_title(title)
+    HTTParty.get("http://boardgamegeek.com/xmlapi/search?search=#{title}&exact=1")
+  end
 
-def id(title)
-  bgg_data_for_title(title)["boardgames"]["boardgame"]["objectid"]
-end
+  def id(title)
+    bgg_data_for_title(title)["boardgames"]["boardgame"]["objectid"]
+  end
 
-def game_found?(title)
-  bgg_data_for_title(title)["boardgames"] != nil && bgg_data_for_title(title)["boardgames"]["boardgame"].is_a?(Hash)
-end
+  def game_found?(title)
+    bgg_data_for_title(title)["boardgames"] != nil && bgg_data_for_title(title)["boardgames"]["boardgame"].is_a?(Hash)
+  end
 
-def game_data(title)
-  HTTParty.get("http://www.boardgamegeek.com/xmlapi/boardgame/#{id(title)}")
-end
+  def game_data(title)
+    HTTParty.get("http://www.boardgamegeek.com/xmlapi/boardgame/#{id(title)}")
+  end
 
-def fetch_game_info(title)
-  begin
-    if game_found?(title)
-      puts "Found one game for #{title}"
+  def fetch_game_info(title)
+    begin
+      if game_found?(title)
+        puts "Found one game for #{title}"
 
-      create_game(title, game_data(title))
-    else
-      puts "ERROR"
-      puts bgg_data_for_title(title)["boardgames"]
+        create_game(title, game_data(title))
+      else
+        puts "ERROR"
+        puts bgg_data_for_title(title)["boardgames"]
+      end
+    rescue URI::InvalidURIError
+      puts "Bad URL tried. Skipping... Tried #{title}"
     end
-  rescue URI::InvalidURIError
-    puts "Bad URL tried. Skipping... Tried #{title}"
   end
 end
--- tests/unit/bgg_test.rb
+++ tests/unit/bgg_test.rb
@@ -3,20 +3,23 @@ require 'test_helper'
 # Tests the BoardGameGeek service.
 class BoardGameGeekTest < Minitest::Test
   def test_finds_one_game
-    fetch_game_info("Agricola")
+    bgg = BoardGameGeek.new
+    bgg.fetch_game_info("Agricola")
 
     refute_nil(Game.find_by_title("Agricola"))
   end
 
   def test_finds_one_game_when_many_would_have_matched
-    fetch_game_info("Carcassonne")
+    bgg = BoardGameGeek.new
+    bgg.fetch_game_info("Carcassonne")
 
     refute_nil(Game.find_by_title("Carcassonne"))
   end
 
   def test_does_not_error_out_if_searches_for_unexpected_characters
+    bgg = BoardGameGeek.new
     expected_output = "Bad URL tried. Skipping... Tried CO₂\n"
 
-    assert_output(expected_output) { fetch_game_info("CO₂") }
+    assert_output(expected_output) { bgg.fetch_game_info("CO₂") }
   end
 end
