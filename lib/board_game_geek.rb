fc58b891aa9b87e20aacd3836b277d4cbf8fb6e5
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-08 09:27:25 -0600

    Extract create_game method

--- lib/board_game_geek.rb
+++ lib/board_game_geek.rb
@@ -1,3 +1,18 @@
+def create_game(title, game_data)
+  @game = Game.new
+
+  @game.title        = title
+  @game.min_players  = game_data["boardgames"]["boardgame"]["minplayers"]
+  @game.max_players  = game_data["boardgames"]["boardgame"]["maxplayers"]
+  @game.min_playtime = game_data["boardgames"]["boardgame"]["minplaytime"]
+  @game.max_playtime = game_data["boardgames"]["boardgame"]["maxplaytime"]
+  @game.age_group    = game_data["boardgames"]["boardgame"]["age"]
+  @game.description  = game_data["boardgames"]["boardgame"]["description"]
+  @game.image        = game_data["boardgames"]["boardgame"]["image"]
+
+  @game.save
+end
+
 def fetch_game_info(title)
   begin
     bgg_data_for_title = HTTParty.get("http://boardgamegeek.com/xmlapi/search?search=#{title}&exact=1")
@@ -9,18 +24,7 @@ def fetch_game_info(title)
 
       game_data = HTTParty.get("http://www.boardgamegeek.com/xmlapi/boardgame/#{id}")
 
-      @game = Game.new
-
-      @game.title        = title
-      @game.min_players  = game_data["boardgames"]["boardgame"]["minplayers"]
-      @game.max_players  = game_data["boardgames"]["boardgame"]["maxplayers"]
-      @game.min_playtime = game_data["boardgames"]["boardgame"]["minplaytime"]
-      @game.max_playtime = game_data["boardgames"]["boardgame"]["maxplaytime"]
-      @game.age_group    = game_data["boardgames"]["boardgame"]["age"]
-      @game.description  = game_data["boardgames"]["boardgame"]["description"]
-      @game.image        = game_data["boardgames"]["boardgame"]["image"]
-
-      @game.save
+      create_game(title, game_data)
     else
       puts "ERROR"
       puts bgg_data_for_title["boardgames"]
